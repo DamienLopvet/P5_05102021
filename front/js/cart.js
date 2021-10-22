e5327ede29cef6a1ad715391397f6a31bb63690e
Damien Lopvet <etwincorp@gmail.com>
2021-10-22 22:32:01 +0200

    validation form, post to API and get orderID

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,13 @@
 (window.onload = setCart()), cartTotalPrice();
 function setCart() {
   allItems = JSON.parse(localStorage.getItem("items")) || [];
+
   if (allItems.length == 0) {
     document.querySelector("h1").innerHTML = "votre panier est vide";
   } else {
     for (let item of allItems) {
       totalOfThisItem = item.price * item.quantity;
-      itemId = item.id;
+
       itemQuantity = item.quantity;
       document.getElementById(
         "cart__items"
@@ -37,44 +38,46 @@ function setCart() {
 
 //Modifying quantity
 
-document.querySelector("section").addEventListener("change", function (e) {
-  if (e.target && e.target.nodeName == "INPUT") {
-    let newQuantity = e.target.closest(".itemQuantity").value;
-    let oldItemID = e.target.closest(".cart__item").id;
-    let oldItemColor = e.target
-      .closest(".cart__item")
-      .querySelector(".color").id;
-    let totalInput = e.target
-      .closest(".cart__item")
-      .querySelector("div.cart__item__content__titlePrice > p");
+document
+  .querySelector("section #cart__items")
+  .addEventListener("change", function (e) {
+    if (e.target && e.target.nodeName == "INPUT") {
+      let newQuantity = e.target.closest(".itemQuantity").value;
+      let oldItemID = e.target.closest(".cart__item").id;
+      let oldItemColor = e.target
+        .closest(".cart__item")
+        .querySelector(".color").id;
+      let totalInput = e.target
+        .closest(".cart__item")
+        .querySelector("div.cart__item__content__titlePrice > p");
 
-    let matchIdAndColor =
-      allItems.find((item) => item.id === oldItemID) &&
-      allItems.find((item) => item.color === oldItemColor);
-    if (matchIdAndColor) {
-      let oldItemIndex = allItems.indexOf(matchIdAndColor);
-      oldItem = allItems[oldItemIndex];
+      let matchIdAndColor =
+        allItems.find((item) => item.id === oldItemID) &&
+        allItems.find((item) => item.color === oldItemColor);
+      if (matchIdAndColor) {
+        let oldItemIndex = allItems.indexOf(matchIdAndColor);
+        oldItem = allItems[oldItemIndex];
 
-      let newItem = {
-        id: oldItem.id,
-        color: oldItem.color,
-        quantity: newQuantity,
-        photo: oldItem.photo,
-        altText: oldItem.altText,
-        price: oldItem.price,
-        name: oldItem.name,
-      };
+        let newItem = {
+          id: oldItem.id,
+          color: oldItem.color,
+          quantity: newQuantity,
+          photo: oldItem.photo,
+          altText: oldItem.altText,
+          price: oldItem.price,
+          name: oldItem.name,
+        };
 
-      allItems.splice(oldItemIndex, 1, newItem);
-      localStorage.setItem("items", JSON.stringify(allItems));
+        allItems.splice(oldItemIndex, 1, newItem);
+        localStorage.setItem("items", JSON.stringify(allItems));
 
-//Fixing new total price of item modified
-      let newTotal = newItem.quantity * newItem.price;
-      totalInput.innerHTML = `${newTotal}`;
+        //Fixing new total price of item modified
+        let newTotal = newItem.quantity * newItem.price;
+        totalInput.innerHTML = `${newTotal}`;
+      }
     }
-  }
-  cartTotalPrice();
-});
+    cartTotalPrice();
+  });
 //delete an Item
 document.querySelector("section").addEventListener("click", function (e) {
   if (e.target && e.target.className == "deleteItem") {
@@ -117,3 +120,54 @@ function cartTotalPrice() {
     .forEach((el) => (totalPrice += +el.textContent));
   document.getElementById("totalPrice").innerText = totalPrice;
 }
+
+//Form input validation
+document
+  .querySelector('.cart__order__form input[type="button"]')
+  .addEventListener("click", function () {
+    //are all inputs valid? let create a variable to checkout
+    var valid = true;
+    for (let input of document.querySelectorAll(".cart__order__form input")) {
+      //corespond a : if (valid = true && input.reportValidity())
+      // qui corespond aussi a : valid = valid && input.reportValidity()
+      valid &= input.reportValidity();
+      if (!valid) {
+        break;
+      }
+    }
+    if (valid) {
+      if (allItems.length >= 1) {
+        itemsId = allItems.map((product) => product.id);
+      }
+      //Send Datas to the API
+      fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          contact: {
+            firstName: document.querySelector('[name="firstName"]').value,
+            lastName: document.querySelector('[name="lastName"]').value,
+            address: document.querySelector('[name="address"]').value,
+            city: document.querySelector('[name="city"]').value,
+            email: document.querySelector('[name="email"]').value,
+          },
+          products: itemsId,
+        }),
+      })
+        .then(function (result) {
+          return result.json();
+        })
+        .then(function (value) {
+          document.querySelector(
+            "main"
+          ).innerHTML = `<div class="title"><h1> Votre commande est en cours </h1>
+          <h2>référence de votre commande : ${value.orderId}</h2></div>`;
+        })
+        .then(function (clear) {
+          localStorage.clear();
+        });
+    }
+  });
