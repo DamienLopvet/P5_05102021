b31e71e017f89c204cdcf71f0bb6d6998b482210
Damien Lopvet <etwincorp@gmail.com>
2021-10-29 08:10:58 +0200

    Typo + size cards css

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -5,7 +5,7 @@ const orderId = new URLSearchParams(window.location.search).get("orderId");
 if (!orderId) {
   document.querySelector(
     ".confirmation"
-  ).innerHTML = `<p>Aucune commande n'a été effectuée pour le moment.</br><a href="index.html"><button type="button">Accueil</button></a></br><a href="cart.html"><button type="button">panier</button></a></p>`;
+  ).innerHTML = `<p>Aucune commande n'a été effectuée pour le moment.</br><a href="index.html"><button type="button">Accueil</button></a></br><a href="cart.html"><button type="button">Panier</button></a></p>`;
 } else {
   document.getElementById("orderId").innerText = orderId;
 }
